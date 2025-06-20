b4422f6d7e68365c376cddef3ced7788323dbf1f
naveen <naveenkumarpenikala@gmail.com>
2025-06-20 15:21:20 +0530

    second commit

--- src/main/java/com/validations/validationsWithGlobalExceptionHandling/EmployeeService.java
+++ src/main/java/com/validations/validationsWithGlobalExceptionHandling/EmployeeService.java
@@ -1,5 +1,6 @@
 package com.validations.validationsWithGlobalExceptionHandling;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -24,6 +25,11 @@ public class EmployeeService {
 	       employee.setCreationDate(new Date());
 		   return EmployeeRepo.save(employee);
 				  
+	}
+
+	public List<Employee> getEmployee() {
+		
+		return EmployeeRepo.findAll();
 	}
 
 }
